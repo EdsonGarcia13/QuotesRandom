ce47bbbb0fb82bec9e6a9fd8c49ce94e264b0814
EDSON DAVID GARCIA CONTRERAS <edson.garcia@saluddigna.it>
2022-05-02 09:02:50 -0600

    modificado

--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -5,12 +5,13 @@ import quotes from './quotes.json';
 const Quotes = () => {
 
     const quotesRandom = quotes[Math.floor(Math.random() * quotes.length)];
-    const [quote, setQuote] = useState(quotesRandom);
+    const [quote, setQuote] = useState([]);
     const aleatorio = () => {
         setQuote(quotesRandom);
     };
 
-    const randomColor = () => {
+
+        const randomColor = () => {
         const colors = ["#FA6A1C", "#0B54A2", "#9722F8", "#3E046E", "#2FDB0D", "#0FCF8A"];
         const color = Math.floor(Math.random() * colors.length);
         return colors[color];
@@ -18,17 +19,15 @@ const Quotes = () => {
     
     const colorize = randomColor();
     document.body.style= `background: ${colorize}`;
+
+  
    
     return (
         <div className='randomQuotes'  style={{color: colorize}}>
             <i className="fa-solid fa-quote-left"></i> 
             <h1> {quote.quote}</h1>
             <h2>{quote.author}</h2>
-            
-            <div>
-            <Buttonn motor={aleatorio}/>
-            </div>
-
+            <Buttonn motor={aleatorio} />
         </div>
     );
 };
